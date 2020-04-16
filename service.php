c909f9da8e64e00797948693417f2c410682f7a9
aasd755326 <38196667+aasd755326@users.noreply.github.com>
2020-04-16 09:38:23 +0800

    Add files via upload

---
+++ service.php
@@ -0,0 +1,17 @@
+<?php
+
+header('Content-Type: application/json; charset=UTF-8'); //設定資料類型為 json，編碼 utf-8
+if ($_SERVER['REQUEST_METHOD'] == "POST") { //如果是 POST 請求
+    @$Input = $_POST["Input"]; //取得  Input POST 值
+    if ($Input != "" ) { 
+        echo json_encode(array(
+            'Input' => $Input
+        )); 
+    }
+} else {
+    //回傳 errorMsg json 資料
+    echo json_encode(array(
+        'errorMsg' => '請求無效，只允許 POST 方式訪問！'
+    ));
+}
+?>
